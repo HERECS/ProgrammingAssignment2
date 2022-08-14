0d7bacb352f2a9b50e422f37d26aee3a03c4f3f5
HERECS <110628696+HERECS@users.noreply.github.com>
2022-08-14 22:59:09 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file contains two functions.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that cache the inverse 
+## of a specified matrix. To illustrate this function, take an example
+## on a matrix, matrix(c(1,2,2,1),2,2). 
+## A <- makeCacheMatrix(matrix(c(1,2,2,1),2,2)) gives a list
+## that store matrix A and its inverse(if have been computed)
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y){
+		x <<- y
+		inv <<- NULL
+	}
 
+	get<- function() x
+	
+	setinverse <- function(inverse) inv <<- inverse
+	getinverse <- function() inv
+
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that compute the inverse of a matrix.
+## If its inverse has been computed, it will return 
+## message "getting cached data". If otherwise, 
+## it compute the inverse and cache it in makeCacheMatrix.
+## This function must be used only after the use of makeCacheMatrix
+## To illustrate this function, back to the previous example 
+## matrix(c(1,2,2,1),2,2). 
+## If we want the inverse of matrix(c(1,2,2,1),2,2),
+## we need to store this matrix in makeCacheMatrix by
+## A <- makeCacheMatrix(matrix(c(1,2,2,1),2,2)),
+## then obtain its inverse by
+## cacheSolve(A).
+## If A does not cache its inverse, then cacheSolve will
+## compute its inverse and cache it in A.
+## Else, cacheSolve(A) return the inverse from A
+## and a message "getting cached data"
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	inv <- x$getinverse()
+	if(!is.null(inv)){
+		message("getting cached data")
+		return(inv)
+	}
+
+	data <- x$get()
+	inv <- solve(data)
+	x$setinverse(inv)
+	inv
+
 }
